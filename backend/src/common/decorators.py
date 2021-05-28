2a3fd503eb5035f1426323588e18ed9b86a5929e
Sunil Pandey <sunil@canvara.com>
2021-05-27 19:35:24 -0700

    Remove files specific to AWS Lambda

--- backend/src/common/decorators.py
+++
@@ -1,6 +0,0 @@
-def register(registry, operation_name):
-    """Accumulate operation name to function mapping"""
-    def _register(func):
-        registry[operation_name] = func
-        return func
-    return _register

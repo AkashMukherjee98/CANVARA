494849d84671f5d0ad7ca8f4d2ba391a511253c2
Sunil Pandey <sunil@canvara.com>
2021-06-10 12:38:01 -0700

    Create initial set of tables - customer, user, post and application

---
+++ backend/src/db/migrations/versions/abc8da851630_create_initial_tables.py
@@ -0,0 +1,62 @@
+"""create initial tables
+
+Revision ID: abc8da851630
+Revises: 
+Create Date: 2021-06-10 11:58:58.753430
+
+"""
+from sqlalchemy.sql.schema import ForeignKey
+from alembic import op
+from sqlalchemy.dialects.postgresql import JSONB, UUID
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'abc8da851630'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'customer',
+        sa.Column('id', UUID, primary_key=True),
+        sa.Column('name', sa.String(255), nullable=False, unique=True),
+    )
+
+    # Note: 'user' is a reserved keyword in postgres so we use 'canvara_user' instead
+    op.create_table(
+        'canvara_user',
+        sa.Column('id', UUID, primary_key=True),
+        sa.Column('customer_id', UUID, ForeignKey('customer.id'), nullable=False),
+        sa.Column('name', sa.String(255), nullable=False),
+        sa.Column('profile', JSONB)
+    )
+
+    op.create_table(
+        'post',
+        sa.Column('id', UUID, primary_key=True),
+        sa.Column('owner_id', UUID, ForeignKey('canvara_user.id'), nullable=False),
+        sa.Column('created_at', sa.DateTime, nullable=False),
+        sa.Column('last_updated_at', sa.DateTime, nullable=False),
+        sa.Column('details', JSONB)
+    )
+
+    # Note: status is a string for now, but may be converted to an Enum later
+    op.create_table(
+        'application',
+        sa.Column('id', UUID, primary_key=True),
+        sa.Column('user_id', UUID, ForeignKey('canvara_user.id'), nullable=False),
+        sa.Column('post_id', UUID, ForeignKey('post.id'), nullable=False),
+        sa.Column('created_at', sa.DateTime, nullable=False),
+        sa.Column('last_updated_at', sa.DateTime, nullable=False),
+        sa.Column('status', sa.String(31), nullable=False),
+        sa.Column('details', JSONB)
+    )
+
+def downgrade():
+    op.drop_table('application')
+    op.drop_table('post')
+    op.drop_table('canvara_user')
+    op.drop_table('customer')

3ba81cdca1b0d30c577e2ff0903be2565637f861
Sunil Pandey <sunil@canvara.com>
2021-06-27 22:16:26 -0700

    Improve Skills searching
    - If no query is given, return a list of overall most used global or customer-specific skills
    - If a query is given, return a list of most used global or customer-specific skill matching
      the query
    - Return up to 10 skills in either case
    - Use substring matching instead of prefix when searching for the given query
    - Make the search case-insensitive

--- src/backend/models/skill.py
+++ src/backend/models/skill.py
@@ -56,9 +56,35 @@ class Skill(ModelBase):
             usage_count=0)
 
     @classmethod
-    def search(cls, tx, query=None):
-        skills = tx.query(Skill).where(Skill.name.startswith(query, autoescape=True))
-        return [skill.as_dict() for skill in skills]
+    def search(cls, tx, customer_id, query=None, limit=10):
+        customer_id_clause = or_(
+            Skill.customer_id == None,
+            Skill.customer_id == customer_id)
+
+        if not query:
+            # If no query was given, return the overall most used skills
+            skills_query = tx.query(Skill).where(customer_id_clause)
+        else:
+            # If a query was given, return the most used skills that contain that query somewhere in the name
+            # TODO: (sunil) Improve this by adding stemming etc.
+            query = query.lower()
+            skills_query = tx.query(Skill).where(
+                and_(
+                    Skill.internal_name.like(f'%{query}%'),
+                    customer_id_clause))
+
+        # It's possible to have a global and a custom skill by the same name
+        # So fetch 2x the number of results we want to return, and then dedupe
+        # This can also be done entirely in SQL, but that results in a much more complex query
+        skills_query = skills_query.order_by(Skill.usage_count.desc()).limit(2*limit)
+        skills = []
+        skill_names_seen = set()
+        for skill in skills_query:
+            if skill.internal_name in skill_names_seen:
+                continue
+            skill_names_seen.add(skill.internal_name)
+            skills.append(skill)
+        return [skill.as_dict() for skill in skills[:limit]]
 
     def as_dict(self):
         return {
--- src/backend/views/skill.py
+++ src/backend/views/skill.py
@@ -1,14 +1,17 @@
 from flask import current_app as app
 from flask import jsonify, request
-from flask_cognito import cognito_auth_required
+from flask_cognito import cognito_auth_required, current_cognito_jwt
 
 from backend.models.db import transaction
 from backend.models.skill import Skill
+from backend.models.user import User
 
 
 @app.route('/skills')
 @cognito_auth_required
 def search_skills_handler():
     with transaction() as tx:
-        skills = Skill.search(tx, query=request.args.get('q'))
+        # TODO: (sunil) Pass current user id to Skill.search and let it join with user table instead of executing two queries
+        user = User.lookup(tx, current_cognito_jwt['sub'])
+        skills = Skill.search(tx, user.customer_id, query=request.args.get('q'))
     return jsonify(skills)

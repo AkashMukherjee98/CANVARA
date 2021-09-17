798eb36cc08440c0a2d501b5978a0521e0aee76b
Prabhnoor Bassi <prabhnoor@canvara.com>
2021-09-17 00:49:59 -0700

    Update customer.py

--- src/backend/views/customer.py
+++ src/backend/views/customer.py
@@ -7,9 +7,13 @@ from sqlalchemy import select
 from backend.models.customer import Customer
 from backend.models.db import transaction
 from backend.views.base import AuthenticatedAPIBase
+from Tools.scripts.patchcheck import status
+from pip._internal.cli import status_codes
+from logging import __status__
 
 
 class CustomerAPI(AuthenticatedAPIBase):
+
     @staticmethod
     def __list_customers():
         with transaction() as tx:
@@ -53,8 +57,13 @@ class CustomerAPI(AuthenticatedAPIBase):
     def delete(customer_id):
         with transaction() as tx:
             customer = Customer.lookup(tx, customer_id, must_exist=False)
+            # if customer does not exist, error code 404 is displayed
             if customer is None:
-                # Noop if the customer does not exist
-                return {}
-            tx.delete(customer)
-        return {}
+                raise NotAllowedError("404 Error, Customer Does Not Exist")
+            else:
+                # delete customer 
+                tx.delete(customer)
+                # return deleted status code, empty response
+                customer.status = Customer.DELETED.value 
+            
+        return make_no_content_response()

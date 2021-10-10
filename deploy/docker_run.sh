dfa57d56d09e4bbbfefa5d9f8cdb51a597663bfa
Sunil Pandey <sunil@canvara.com>
2021-10-10 00:05:14 -0700

    Fix a typo in the directory name for db migrations

--- deploy/docker_run.sh
+++ deploy/docker_run.sh
@@ -1,3 +1,2 @@
-cd src/backend/db
-alembic upgrade head
+alembic -c /canvara/backend/db/alembic.ini upgrade head
 gunicorn --bind 0.0.0.0:8000 --workers 2 --capture-output --access-logfile /canvara/logs/api_access.log --error-logfile /canvara/logs/api_error.log 'backend:create_app()'

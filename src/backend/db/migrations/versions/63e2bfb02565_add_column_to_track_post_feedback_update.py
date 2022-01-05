e9d77d46fec7f3a35368f56342e970ba3df97434
Santanu Brahma <santanu.brahma@cbnits.com>
2022-01-05 23:27:17 +0530

    Update migration down version

--- src/backend/db/migrations/versions/63e2bfb02565_add_column_to_track_post_feedback_update.py
+++ src/backend/db/migrations/versions/63e2bfb02565_add_column_to_track_post_feedback_update.py
@@ -1,7 +1,7 @@
 """add column to track post feedback update
 
 Revision ID: 63e2bfb02565
-Revises: 27855dec2c30
+Revises: 534852b7364a
 Create Date: 2022-01-05 15:35:28.552195
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '63e2bfb02565'
-down_revision = '27855dec2c30'
+down_revision = '534852b7364a'
 branch_labels = None
 depends_on = None
 

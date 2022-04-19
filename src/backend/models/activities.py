146c5d5ac5b38a647e5ca9892c9810826b0f3f98
Santanu Brahma <93534367+santanu-canvara@users.noreply.github.com>
2022-04-19 17:58:00 +0530

    Revert my activities to ensure its working for other users

--- src/backend/models/activities.py
+++ src/backend/models/activities.py
@@ -1,16 +1,22 @@
+from sqlalchemy import or_
+
 from backend.models.position import Position
 
-from .offer import Offer
+from .user import User
+from .post import Post
+from .application import Application
+from .offer import Offer, OfferProposal
+from .community import Community, CommunityMembership
+from .event import Event, EventRSVP
 
 
 class MyActivity():
     @classmethod
-    def own_gigs(cls, tx, user):  # pylint: disable=unused-argument
-        '''gigs = tx.query(Post).where(
+    def own_gigs(cls, tx, user):
+        gigs = tx.query(Post).where(
             Post.owner_id == user.id
         )
-        return list(gigs)'''
-        return list()
+        return list(gigs)
 
     @classmethod
     def own_offers(cls, tx, user):
@@ -27,64 +33,58 @@ class MyActivity():
         return list(positions)
 
     @classmethod
-    def act_gigs(cls, tx, user, status):  # pylint: disable=unused-argument
-        '''gigs = tx.query(Post).join(Application).where(
+    def act_gigs(cls, tx, user, status):
+        gigs = tx.query(Post).join(Application).where(
             Application.user_id == user.id
         )
         if status:
             gigs = gigs.where(Application.status.in_(status))
-        return list(gigs)'''
-        return list()
+        return list(gigs)
 
     @classmethod
-    def my_applications(cls, tx, user, status):  # pylint: disable=unused-argument
-        '''applications = tx.query(Application).where(
+    def my_applications(cls, tx, user, status):
+        applications = tx.query(Application).where(
             Application.user_id == user.id,
             Application.status.in_(status)
         )
-        return list(applications)'''
-        return list()
+        return list(applications)
 
     @classmethod
-    def my_proposals(cls, tx, user, status):  # pylint: disable=unused-argument
-        '''proposals = tx.query(OfferProposal).join(Offer).where(
+    def my_proposals(cls, tx, user, status):
+        proposals = tx.query(OfferProposal).join(Offer).where(
             Offer.offerer_id == user.id or
             OfferProposal.proposer_id == user.id,
             OfferProposal.status.in_(status)
         )
-        return list(proposals)'''
-        return list()
+        return list(proposals)
 
     @classmethod
-    def my_communities(cls, tx, user):  # pylint: disable=unused-argument
-        '''communities = tx.query(Community).join(CommunityMembership.member).where(or_(
+    def my_communities(cls, tx, user):
+        communities = tx.query(Community).join(CommunityMembership.member).where(or_(
             Community.primary_moderator_id == user.id,
             Community.secondary_moderator_id == user.id,
             Community.status == 'active' and
             CommunityMembership.member_id == user.id
         ))
-        return list(communities)'''
-        return list()
+        return list(communities)
 
     @classmethod
-    def my_events(cls, tx, user):  # pylint: disable=unused-argument
-        '''events = tx.query(Event).join(EventRSVP.guest).where(or_(
+    def my_events(cls, tx, user):
+        events = tx.query(Event).join(EventRSVP.guest).where(or_(
             Event.primary_organizer_id == user.id,
             Event.secondary_organizer_id == user.id,
             Event.status == 'active' and
             EventRSVP.member_id == user.id
         ))
-        return list(events)'''
-        return list()
+        return list(events)
 
     @classmethod
-    def my_connections(cls, tx, user):  # pylint: disable=unused-argument
-        '''users = tx.query(User).join(CommunityMembership).where(
+    def my_connections(cls, tx, user):
+        users = tx.query(User).join(CommunityMembership).where(
             CommunityMembership.member != user,
             CommunityMembership.member_id == User.id,
             CommunityMembership.community_id.in_(tx.query(CommunityMembership.community_id).where(
                 CommunityMembership.member == user
             ).subquery())
         )
-        return list(users)'''
-        return list()
+        return list(users)

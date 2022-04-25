6c36f5b8c06eb616a5b20aff5dcd7bb3613002bd
Santanu Brahma <santanu.brahma@cbnits.com>
2022-04-26 01:34:44 +0530

    Code tweak and testing done for performer details

--- src/backend/models/performer.py
+++ src/backend/models/performer.py
@@ -32,6 +32,8 @@ class Performer(ModelBase):
     __tablename__ = 'performer'
 
     application = relationship("Application")
+    # This is a hack to fetch application->applicant->profile_picture
+    applicant = relationship("User", secondary='application')
 
     @classmethod
     def lookup_by_post(cls, tx, post_id):
@@ -48,11 +50,14 @@ class Performer(ModelBase):
     def lookup(cls, tx, post_id, performer_id, must_exist=True):
         query_options = [
             contains_eager(cls.application).joinedload(Application.post, innerjoin=True),
-            contains_eager(cls.application).joinedload(Application.applicant, innerjoin=True)
+            contains_eager(cls.application).joinedload(Application.applicant, innerjoin=True),
+            # This is a hack to fetch application->applicant->profile_picture
+            contains_eager(cls.applicant).joinedload(User.profile_picture, innerjoin=True)
         ]
 
         performer = tx.query(cls).join(cls.application).join(Application.post).join(Application.applicant).where(and_(
-            Post.id == post_id
+            Post.id == post_id,
+            User.id == performer_id
         )).options(query_options).one_or_none()
 
         if performer is None and must_exist:
@@ -63,6 +68,6 @@ class Performer(ModelBase):
         return {
             'post_id': self.application.post.id,
             'user_id': self.application.applicant.id,
-            'performer': self.application.applicant.profile_picture_url,
+            'performer': self.application.applicant.as_summary_dict(),
             'status': self.status
         }
